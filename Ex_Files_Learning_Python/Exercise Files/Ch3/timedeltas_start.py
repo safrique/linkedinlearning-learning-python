0314b667bcd1c4d4a14a8d3fbbe51eae5e18a624
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-21 15:28:11 +0100

    time deltas

--- Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
@@ -5,30 +5,39 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 
 # construct a basic timedelta and print it
-
+# print(timedelta(days=365, hours=5, minutes=1))
 
 # print today's date
+# now = datetime.now()
+# print("today is :" + str(now))
 
+# # print today's date one year from now
+# print("One year from now it will be: " + str(now + timedelta(days=365)))
 
-# print today's date one year from now
+# # create a timedelta that uses more than one argument
+# print("In 2 days and 3 weeks, it will be " +
+#       str(now + timedelta(days=2, weeks=3)))
 
+# # calculate the date 1 week ago, formatted as a string
+# # print("1 week ago it was: " + str(now - timedelta(weeks=1)))
+# t = datetime.now() - timedelta(weeks=1)
+# s = t.strftime("%A %d %B %Y")
+# print("1 week ago it was: " + s)
 
-# create a timedelta that uses more than one argument
-
-
-# calculate the date 1 week ago, formatted as a string
-
-
-### How many days until April Fools' Day?
-
+# How many days until April Fools' Day?
+today = date.today()
+afd = date(today.year, 4, 1)
 
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
+if afd < today:
+    print("April fool's day already went by %d ago" % ((today-afd).days))
+    afd = afd.replace(year=today.year + 1)
 
-
-# Now calculate the amount of time until April Fool's Day  
-
-
+# Now calculate the amount of time until April Fool's Day
+time_to_afd = afd- today
+print("It's just", time_to_afd.days, "days until April fool's day")
