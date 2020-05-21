1fadf27028772a858a90bd889df9a6f06a6c2d33
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-21 18:01:46 +0100

    calendar

--- Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
@@ -3,24 +3,47 @@
 #
 
 # import the calendar module
-
+import calendar
+from datetime import date
 
 # create a plain text calendar
-
+d = date.today()
+c = calendar.TextCalendar(calendar.SUNDAY)
+# st = c.formatmonth(d.year, d.month, 0, 0)
+# print(st)
 
 # create an HTML formatted calendar
-
+# hc = calendar.HTMLCalendar(calendar.SUNDAY)
+# st = hc.formatmonth(d.year, d.month)
+# print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
 
-  
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+# for i in c.itermonthdays(d.year, d.month):
+#     print(i)
+# for name in calendar.month_name:
+#     print(name)
 
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
-  
+print("Team meetings will be on: ")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(d.year, m)
+    # print(cal)
+    weekone = cal[0]
+    weektwo = cal[1]
+    # print(weekone)
+    # print(weekone[calendar.FRIDAY])
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+    print("%10s %2d" % (calendar.month_name[m], meetday))
--- Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
@@ -35,7 +35,7 @@ afd = date(today.year, 4, 1)
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
 if afd < today:
-    print("April fool's day already went by %d ago" % ((today-afd).days))
+    print("April fool's day already went by %d days ago" % ((today-afd).days))
     afd = afd.replace(year=today.year + 1)
 
 # Now calculate the amount of time until April Fool's Day
