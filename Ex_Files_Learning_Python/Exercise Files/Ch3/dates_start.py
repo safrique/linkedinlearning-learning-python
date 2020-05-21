c64c934012548e36549179d0fb8697d9c9e3629d
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-21 15:02:17 +0100

    basic datetime

--- Ex_Files_Learning_Python/Exercise Files/Ch3/dates_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch3/dates_start.py
@@ -1,27 +1,35 @@
 #
 # Example file for working with date information
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
-  ## DATE OBJECTS
-  # Get today's date from the simple today() method from the date class
+    # DATE OBJECTS
+    # print(date)
+    # Get today's date from the simple today() method from the date class
+    # today = date.today()
+    # print("Today's date is", today)
+
+    # # print out the date's individual components
+    # print("Date components:", today.day, today.month, today.year)
+
+    # # retrieve today's weekday (0=Monday, 6=Sunday)
+    # print("Today's weekday number is:", today.weekday())
+    # days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    # print("Which is a", days[today.weekday()])
+
+    # DATETIME OBJECTS
+    # Get today's date from the datetime class
+    today = datetime.now()
+    print(today)
+
+    # Get the current time
+    t = datetime.time(datetime.now())
+    print(t)
 
 
-  # print out the date's individual components
-
-  
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-
-  
-  ## DATETIME OBJECTS
-  # Get today's date from the datetime class
-
-
-  # Get the current time
-
-
-  
 if __name__ == "__main__":
-  main();
-  
+    main()
