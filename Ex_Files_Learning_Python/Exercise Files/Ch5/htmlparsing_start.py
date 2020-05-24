4a89cd0246ce35dbd43ac60b751f8ff40f2df74f
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-24 09:44:39 +0100

    html

--- Ex_Files_Learning_Python/Exercise Files/Ch5/htmlparsing_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch5/htmlparsing_start.py
@@ -1,13 +1,59 @@
-# 
+#
 # Example file for parsing and processing HTML
 #
 
+from html.parser import HTMLParser
+import os
+
+metacount = 0
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        print("*** Encountered comment:", data)
+        pos = self.getpos()
+        print("\tAt line:", pos[0], "position", pos[1])
+
+    def handle_starttag(self, tag, attrs):
+        global metacount
+        if tag == 'meta':
+            metacount += 1
+
+        print("*** Encountered start tag:", tag)
+        pos = self.getpos()
+        print("\tAt line:", pos[0], "position", pos[1])
+
+        if attrs.__len__() > 0:
+            print("\tAttributes:")
+            for a in attrs:
+                print("\t", a[0], "=", a[1])
+
+    def handle_endtag(self, tag):
+        print("*** Encountered end tag:", tag)
+        pos = self.getpos()
+        print("\tAt line:", pos[0], "position", pos[1])
+
+    def handle_data(self, data):
+        if (data.isspace()):
+            return
+
+        print("*** Encountered data:", data)
+        pos = self.getpos()
+        print("\tAt line:", pos[0], "position", pos[1])
+
 
 def main():
-  # instantiate the parser and feed it some HTML
-  parser = MyHTMLParser()
-    
+    # setting working directory
+    script_dir = os.path.dirname(__file__)
+
+    # instantiate the parser and feed it some HTML
+    parser = MyHTMLParser()
+    f = open(os.path.join(script_dir, "samplehtml.html"), "r")
+    if f.mode == "r":
+        contents = f.read()
+        parser.feed(contents)
+        print("Total number of metatags found: " + str(metacount))
+
 
 if __name__ == "__main__":
-  main();
-  
+    main()
